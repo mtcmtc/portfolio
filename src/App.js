42c4ff26f1f572b7bbfb116376e9306a84a420fe
mtcmtc <mi2q25q@l-sff21bg8wm-m.gap.com>
2019-03-26 17:33:19 -0700

    route path fix to /portfolio/

--- src/App.js
+++ src/App.js
@@ -52,10 +52,10 @@ export default class App extends Component {
         <GlobalStyle />
         <Nav />
         <div className="wrapper">
-          { <Route exact={true} path="/" component={Home}/> }
-          {/* <Route path="/resume" component={Resume}/> */}
-          {/* <Route path="/projects" component={Projects}/> */}
-          {/* <Route path="/contact" component={Contact}/> */}
+          { <Route exact={true} path="/portfolio/" component={Home}/> }
+          {/* <Route path="/portfolio/resume" component={Resume}/> */}
+          {/* <Route path="/portfolio/projects" component={Projects}/> */}
+          {/* <Route path="/portfolio/contact" component={Contact}/> */}
 
         </div>
       </Router>
